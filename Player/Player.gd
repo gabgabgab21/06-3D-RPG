5fb2dc614b740fc7818904bd2049b819ec6c9d34
gabgabgab21 <122471903+gabgabgab21@users.noreply.github.com>
2023-04-25 21:50:28 -0400

    completes exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+onready var Camera = get_node("/root/Game/Player/Pivot/Camera")
+
 func _physics_process(_delta):
 	pass
 	
@@ -7,3 +9,12 @@ func get_input():
 	var input_dir = Vector3()
 	if Input.is_action_pressed("forward"):
 		input_dir -= Camera.global_transform.basis.z
+	if Input.is_action_pressed("back"):
+		input_dir += Camera.global_transform.basis.z
+	if Input.is_action_pressed("left"):
+		input_dir -= Camera.global_transform.basis.x
+	if Input.is_action_pressed("right"):
+		input_dir += Camera.global_transform.basis.x
+	input_dir = input_dir.normalized()
+	return input_dir
+		
