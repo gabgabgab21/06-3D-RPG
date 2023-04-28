c60bc6cea7e3c80d282b28a4b838abbf688bc071
gabgabgab21 <122471903+gabgabgab21@users.noreply.github.com>
2023-04-28 19:24:52 -0400

    completes exercise

--- UI/Dialogue.gd
+++ UI/Dialogue.gd
@@ -9,9 +9,15 @@ func _ready():
 	hide()
 	
 func _physics_process(_delta):
-	if dialogue.size() and dialogue_pos < dialogue_size():
+	if dialogue.size() and dialogue_pos < dialogue.size():
 		var d = "[center]" + dialogue[dialogue_pos] + "[/center]"
-		$Text.
+		if $Text.bbcode_text != d:
+			$Text.bbcode_text = d
+		if Input.is_action_just_pressed("action"):
+			dialogue_pos += 1
+	elif dialogue.size():
+		emit_signal("finished_dialogue")
+		hide_dialogue()
 
 func start_dialogue(d):
 	dialogue = d.duplicate()
